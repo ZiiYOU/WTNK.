490e61a03cb445a8a2353343a5d3d66231f0207a
Jiyoung <jiyoung990602@gmail.com>
2025-01-18 04:13:34 +0900

    🎉 add : Layout 설정

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "../pages/Main";
+import Layout from "../layout/Layout";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Main />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
